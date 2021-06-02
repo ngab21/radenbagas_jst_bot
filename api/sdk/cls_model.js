79b1c8d7a11a08a9a4ebd439580a868412b95317
ngab21 <84616551+ngab21@users.noreply.github.com>
2021-06-02 12:55:48 +0700

    Update cls_model.js

--- api/sdk/cls_model.js
+++ api/sdk/cls_model.js
@@ -31,7 +31,7 @@ async function classyfy(data){
 
     try{
         // path load in public access => github
-        const path = 'https://raw.githubusercontent.com/ngab21/jst-bagas-bot/main/public/ex_model/model.json;
+        const path = 'https://raw.githubusercontent.com/anzala0807/bot_jst_anzala-/main/public/ex_model/model.json;
         const model = await tf.loadGraphModel(path);
         
         predict = model.predict(
